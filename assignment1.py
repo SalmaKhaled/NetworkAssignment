74513c1d5a723ab1301c05b8e98f258a975c1374
Amira Ayman <amiraayman950@gmail.com>
2018-11-22 15:52:36 +0200

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -31,3 +31,36 @@ def CRC(frame,genterator):
 	#print(result)
     #print(message)
     return message
+
+#ALTER FUNCTION
+def alter(message):
+    index = input("Please enter the index:")
+    x = int(index) - 1
+    toinvert = message[x]
+    # message = message.replace(toinvert,str(int(toinvert)^1))
+    message = message[:x] + str(int(toinvert) ^ 1) + message[x + 1:]
+    # print(message)
+    return message
+
+#MAIN
+""" input """
+f = open("input.txt", 'r')
+f_out = open("out.txt", 'w')
+lines = f.readlines()
+frame = lines[0].replace('\n', '')
+genterator = lines[1].replace('\n', '')
+
+message = CRC(frame, genterator)
+f_out.write(message)
+f_out.close()
+f_out = open("out.txt", 'r')
+message2=f_out.read()
+verifier(message2, genterator)
+
+message3 = alter(message2)
+f_out = open("out_alter.txt", 'w')
+f_out.write(message3)
+f_out.close()
+f_out = open("out_alter.txt", 'r')
+msg=f_out.read()
+verifier(msg, genterator)
